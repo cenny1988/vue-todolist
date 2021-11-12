bb656d43e13b55b7dd31033bd840ab74c698d672
cenny1988 <59100992+cenny1988@users.noreply.github.com>
2021-11-12 15:35:48 +0100

    creato ar obj tasks e metodo per rimuovere

--- js/main.js
+++ js/main.js
@@ -1,1 +1,39 @@
-console.log('hei there!');
+/*
+- stampare in pagina un item per ogni elemento contenuto in un array
+- ogni item ha una “x” associata: cliccando su di essa, l’item viene rimosso dalla lista
+- predisporre un input per aggiungere un nuovo item alla lista: digitando il tasto invio oppure ciccando su un pulsante, il testo digitato viene aggiunto alla lista
+ATTENZIONE PERO’!!!!
+il vostro array sarà un array di oggetti;
+gli oggetti avranno questo formato:
+{ text: 'Fare la spesa', done: true }, 
+{ text: 'Fare il bucato', done: false }
+etc. etc..
+quindi nell’elenco di output ciò che sara “done” sarà barrato, il resto invece con testo normale.
+Nella richiesta base mi limito al fatto che quel “done” nei task inseriti dall’utente sarà sempre false,
+gli unici true saranno alcuni nei dati di partenza.
+BONUS
+Gesticso i done dall’interfaccia,
+quindi il task potrà essere anche segnato come done e non per forza cancellato
+poi se l’utente vuole potrà anche cancellarlo del tutto.
+*/
+var app = new Vue({
+    el: '#myapp',
+    data: {
+        tasks: [
+            {
+                text: 'Fare la spesa',
+                done: true
+            },
+            {
+                text: 'Fare la bucato',
+                done: false
+            },
+        ]
+    },
+    methods: {
+        removeTask(index){
+            this.tasks.splice(index,1);
+        }
+        
+    },
+});
